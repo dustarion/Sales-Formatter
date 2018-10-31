3212097916c6cab2f11338a3005aadf9c3fa76aa
Dalton Ng <dustarion@gmail.com>
2018-10-31 17:20:59 +0800

    Initial Commit

---
+++ Ca1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Ca1
+//
+//  Created by Dalton Ng on 31/10/18.
+//  Copyright © 2018 Dalton Prescott. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
